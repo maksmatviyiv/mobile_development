312caf2e6dfb7a20174c0c6dbc947d516c259f56
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2018-11-27 22:11:14 +0200

    labwork1

---
+++ app/src/main/java/com/example/maksy/mobile_development/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.maksy.mobile_development;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("word", "hello world");
+        System.out.println("Hello world");
+    }
+}
